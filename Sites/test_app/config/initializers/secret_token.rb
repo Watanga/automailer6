2c488235c6177dae6a4347767bba5fce2a3f8249
Gustavo Milstein <milstein.gustavo@gmail.com>
2012-07-28 00:42:42 -0300

    Finish user signup

---
+++ Sites/test_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestApp::Application.config.secret_token = '542cc29f0ff2aba034aff138e20a7836e2a398524a90d32f679e8edb9510823883a81d8e89c228f5155cd22622541054e40d65b2b57173f5a98cab0146b067cf'
